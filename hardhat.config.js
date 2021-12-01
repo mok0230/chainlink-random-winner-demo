1604be70df749f9f0919adb9ffd3bc1485255161
mok <mok0230@protonmail.com>
2021-12-01 11:32:30 -0800

    remove rinkeby network and update compiler version

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,15 +2,11 @@ require('@nomiclabs/hardhat-waffle');
 require('dotenv').config();
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.7",
   networks: {
     kovan: {
       url: `${process.env.ALCHEMY_KOVAN_URL}`,
       accounts: [`0x${process.env.KOVAN_PRIVATE_KEY}`],
-    },
-    rinkeby: {
-      url: `${process.env.ALCHEMY_RINKEBY_URL}`,
-      accounts: [`0x${process.env.RINKEBY_PRIVATE_KEY}`],
-    } 
+    }
   }
 };
